e12e888d5c658b08221fe8a558551c6b7f2800fe
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-21 19:47:17 +0900

    Use Travis env var to specify forked repository.

--- script/run_travis_image.sh
+++ script/run_travis_image.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 echo "target redmine version: ${REDMINE_VER}"
-echo "target branch: ${TRAVIS_BRANCH}"
+echo "target plugin repository: ${TRAVIS_REPO_SLUG}"
+echo "target plugin branch: ${TRAVIS_BRANCH}"
 git clone --depth=1 --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
 cp /database.yml /redmine/config/
-git clone --depth=1 --branch=${TRAVIS_BRANCH} https://github.com/akiko-pusu/redmine_issue_templates.git /redmine/plugins/redmine_issue_templates
+git clone --depth=1 --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_issue_templates
 mv /redmine/plugins/redmine_issue_templates/Gemfile.local /redmine/plugins/redmine_issue_templates/Gemfile
 cd /redmine
 bundle install --without rmagick
